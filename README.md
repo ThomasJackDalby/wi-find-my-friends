b4709678d065e3faf541b72b58c39ea48323d0be
Tom Dalby <thomasjackdalby@gmail.com>
2026-07-16 06:34:59 +0100

    Updating doc strings and removed postgres as dependency

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # WiFi<sup>nd</sup> My Friends
 
 The 'WiFi<sup>nd</sup> My Friends' app uses WiFi as an approximate WPS (WiFi Positioning System). The app scans nearby WiFI networks and uses the RSSI values for network SSIDs that are known to have static access points (APs). If enough APs are detected in a scan, the badges position can be triangulated.
+
+
+## Credits
+
+The following repos/projects were invaluable in the development of WiFi<sup>nd</sup> My Friends.
+
+- https://github.com/webboggles/tildagon-wifi-radar [@webboggles](https://github.com/webboggles) &mdash; [weborder.uk](https://weborder.uk)
+
+## Licence
+
+MIT
--- server/utils.py
+++ server/utils.py
@@ -1,14 +1,23 @@
 import numpy as np
+import socket
+import requests
 from scipy.optimize import minimize
 
-# def _rssi_to_dist(rssi):
-#     if rssi >= 0:
-#         return 0.5
-#     return min(MAX_RANGE_M, 10.0 ** ((TX_POWER - rssi) / (10.0 * PATH_LOSS_N)))
+SCAN_PERIOD   = 1.0
+TX_POWER      = -40
+PATH_LOSS_N   = 2.8
+MAX_RANGE_M   = 30.0
+
+def rssi_to_dist(rssi):
+    if rssi >= 0: return 0.5
+    return min(MAX_RANGE_M, 10.0 ** ((TX_POWER - rssi) / (10.0 * PATH_LOSS_N)))
 
 def estimate_location(signals: list[tuple[float, float, float]]) -> tuple[float, float]:
     """
-    
+    Estimates the position based on signals received.
+
+    Returns:
+        tuple[float, float]: A tuple of the x, y position (e.g. (1.23, 4.56))
     """
     anchors = np.array([[s[0], s[1]] for s in signals])
 
@@ -25,3 +34,35 @@ def estimate_location(signals: list[tuple[float, float, float]]) -> tuple[float,
         method='BFGS' # Broyden–Fletcher–Goldfarb–Shanno algorithm
     )
     return result.x
+
+def get_local_ip():
+    """
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80)) # Doesn't have to be reachable
+        local_ip = s.getsockname()[0]
+    except Exception:
+        local_ip = '127.0.0.1'
+    finally:
+        s.close()
+    return local_ip
+
+def get_external_ip() -> str:
+    """
+    Retrieves the public IP address of the host machine.
+
+    Queries an external IP lookup service to determine the current public-facing 
+    IPv4 address and returns it as a standard dot-separated string.
+
+    Returns:
+        str: The public IP address (e.g., '192.0.2.1').
+
+    Raises:
+        requests.RequestException: If the network request to the external 
+            API fails or times out.
+        ValueError: If the retrieved response is not a valid IPv4 address.
+    """
+    response = requests.get('https://api.ipify.org').text
+    # TODO: assert the text is of the correct form
+    return response
